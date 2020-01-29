6270b7030ddd579fc1bde1f195de3e3dc251c81b
Richard Todd <rwtodd@users.noreply.github.com>
2020-01-29 15:42:44 -0600

    added CSV extract option for WORDS.TOK

--- cmd/agi-extract/extract.go
+++ cmd/agi-extract/extract.go
@@ -4,14 +4,28 @@ import (
 	"fmt"
 	"os"
 	"sort"
+	"strings"
 
 	"github.com/rwtodd/agi-tools/agi"
 	flag "github.com/rwtodd/cmdflag"
 )
 
-// print the inverse map, from token type to
-// a list of words...
-func formatWords(w map[string]uint16) {
+// format the WORDS.TOK data as CSV, sorted by word
+func formatWordsCSV(w map[string]uint16) {
+	fmt.Println(`"Word","Category"`)
+	var keys = make([]string, 0, len(w))
+	for k := range w {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+	for _, k := range keys {
+		fmt.Printf("\"%s\",\"%d\"\n", k, w[k])
+	}
+}
+
+// format the WORDS.TOK data as a text table, ordered by the
+// category number.
+func formatWordsTable(w map[string]uint16) {
 	var invMap = make(map[uint16][]string)
 	for k, v := range w {
 		invMap[v] = append(invMap[v], k)
@@ -35,16 +49,20 @@ func formatWords(w map[string]uint16) {
 
 func main() {
 	ver := flag.Bool("ver", false, "Print AGI Version Number")
-	words := flag.Bool("words", false, "Print Words.TOK contents")
+	words := flag.String("words", "NONE", "Print Words.TOK contents (arg can be CSV|TABLE)")
 
 	flag.Parse()
+	*words = strings.ToUpper(*words)
+
+	// set the root directory of the game
 	rootDir := "."
 	if nonFlags := flag.Args(); len(nonFlags) > 0 {
 		rootDir = nonFlags[0]
 	}
 
+	// determine the index loading options based on the flags provided
 	var options agi.GameLoadOption
-	if *words {
+	if *words != "NONE" {
 		options |= agi.Load_WordsTok
 	}
 
@@ -54,10 +72,14 @@ func main() {
 		os.Exit(1)
 	}
 
+	// produce the requested output
 	if *ver {
 		fmt.Println("AGI Version", &game.Version)
 	}
-	if *words {
-		formatWords(game.Words)
+	switch *words {
+	case "CSV":
+		formatWordsCSV(game.Words)
+	case "TABLE":
+		formatWordsTable(game.Words)
 	}
 }
