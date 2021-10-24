a906572d0045405d46f8777ebb07fd57809ba070
Richard Todd <rwtodd@users.noreply.github.com>
2021-10-24 01:12:52 -0500

    Adding resource directories and prefix determination
    Now there is a load-game-info high-level function to validate
    a game path, determine its version, and load the resource
    directories.

--- src/org_rwtodd/agi/resources.clj
+++ src/org_rwtodd/agi/resources.clj
@@ -1,6 +1,8 @@
 (ns org-rwtodd.agi.resources
   (:require [clojure.java.io :as io]))
 
+
+;; ====== Validation and Utilties
 (defn game-path?
   "Verify that this appears to be an AGI game directory"
   [path]
@@ -8,9 +10,16 @@
     (and (.isDirectory dir)
          (.exists (io/file dir "AGIDATA.OVL")))))
 
+(defn- read-entire-file
+  "Reads an entire file into a byte array"
+  [path fname]
+  (with-open [stream (->> fname (io/file path) io/input-stream)]
+    (.readAllBytes stream)))
+
+;; ====== Game Version Info
 ;; ---- support functions for `extract-version` -------------
 (defn- digit? [x] (<= 48 x 57)) ;; 48-57 == ASCII 0-9
-(defn- dot?   [x] (== x 46))  ;; 46 == ASCII '.'
+(defn- dot?   [x] (== x 46))    ;; 46 == ASCII '.'
 (def ^:private  long-vernum [digit? dot? digit? digit? digit? dot? digit? digit? digit?])
 (def ^:private short-vernum  [digit? dot? digit? digit? digit?])
 (defn- vermatch [ver bs]  (every? true? (map #(%1 %2) ver bs)))
@@ -43,3 +52,65 @@
   (Double/parseDouble (if (== (count sv) 9)
                         (str (.substring sv 0 5) (.substring sv 6))
                         sv)))
+
+(defn load-version
+  "Determine the game's AGI version based on the AGIDATA.OVL file."
+  [path]
+  (-> (read-entire-file path "AGIDATA.OVL") extract-version numeric-version))
+
+(defn determine-v3-prefix
+  "Version 3 AGI games have a prefix on their resource files, and we can
+  figure out what it is by inspecting the file system.  Needs the PATH to
+  the root directory of the game."
+  [path]
+  (let [fls    (file-seq (io/file path))
+        volf   (first (filter #(.endsWith (.getName %) "VOL.0") fls))
+        volstr (and volf (.getName volf))
+        pfx    (and volf (.substring volstr 0 (- (.length volstr) 5)))]
+    (if (.exists (io/file path (str pfx "DIR")))
+      pfx
+      (throw (ex-info "Can't determine game prefix!" {:dir path})))))
+
+;; ====== Resource Directories
+(defn- split-v3-dir
+  "Split a version-3 resource directory into sound, view, pic, and logic portions."
+  [dirfl]
+  { :sounds nil :logics nil :views nil :pics nil })
+
+(def ^:private v2-directories
+  { :logics "LOGDIR", :sounds "SNDDIR", :views "VIEWDIR", :pics "PICDIR" })
+
+(defn load-resource-directories
+  "Returns a map of :sound, :view, :pic, and :logic resource directories from the
+  game's directory in PATH.  How this is done depends no the game version.  For V3
+  directories, you must pass the game's PREFIX."
+  ([path]
+   (zipmap (keys v2-directories)
+           (map #(read-entire-file path %) (vals v2-directories))))
+  ([path prefix]
+   (split-v3-dir (read-entire-file path (str prefix "DIR")))))
+
+;; ====== High-Level Interface
+
+(defn load-game-info
+  "This is the main high-level function in this package, which verifies the
+  given PATH points to an AGI game, and loads version and resource directory
+  information into a map.  This map will be used by the other functions to
+  load resources."
+  [path]
+  (if (game-path? path)
+    (let [ver (load-version path)
+          pfx (when (>= ver 3.0) (determine-v3-prefix path))]
+      (merge { :version ver, :path (io/file path) }
+             (if pfx
+               (load-resource-directories path pfx)
+               (load-resource-directories path))
+             (and pfx { :prefix pfx })))
+
+    (throw (ex-info "Not a game directory!" {:dir path}))))
+
+
+;; ====== end of file
+;; Local Variables:
+;; page-delimiter: "^;; ======"
+;; End:
